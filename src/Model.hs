1da9fea087086f433e6b37cb78d3d11c0211b797
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-04-11 22:16:18 +1000

    Cleaned up.

--- src/Model.hs
+++ src/Model.hs
@@ -9,11 +9,10 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Database where
+module Model where
 
-import Prelude hiding (tail)
-import qualified Prelude
-import Data.Text (Text, pack, unpack, tail, breakOn)
+import Data.Text (Text, pack, unpack, breakOn)
+import qualified Data.Text as T
 import Database.Persist.TH
 import Language.Haskell.TH.Syntax
 
@@ -21,11 +20,9 @@ import Database.Persist.MongoDB
 import Network (PortID (PortNumber))
 import Network.URI
 
-let mongoSettings =
-        (mkPersistSettings (ConT ''MongoBackend)) {
-            mpsGeneric = False
-         }
- in share [mkPersist mongoSettings] [persistLowerCase|
+share [mkPersist (mkPersistSettings (ConT ''MongoBackend)) {
+    mpsGeneric = False
+ }] [persistLowerCase|
 Graph
     ident Int
     config Text
@@ -40,21 +37,21 @@ parseDatabaseUrl durl =
             Just uri -> if uriScheme uri /= "mongodb:"
                 then schemeError uri
                 else case uriAuthority uri of
-                    Nothing   -> invalid
-                    Just a -> (a, uriPath uri)
+                    Nothing -> invalid
+                    Just a  -> (a, uriPath uri)
         (user,password) = userAndPassword auth
      in
         -- Prelude.tail is unsafe but these settings should always exist.
          [(pack "user",     user)
-         ,(pack "password", tail password)
+         ,(pack "password", T.tail password)
          ,(pack "host",     pack $ uriRegName auth)
-         ,(pack "port",     pack $ Prelude.tail $ uriPort auth)
-         ,(pack "dbname",   pack $ Prelude.tail $ dbpath)
+         ,(pack "port",     pack $ tail $ uriPort auth)
+         ,(pack "dbname",   pack $ tail $ dbpath)
          ]
   where
     -- Prelude.init is not safe, but should be there on Heroku
     userAndPassword :: URIAuth -> (Text, Text)
-    userAndPassword = (breakOn $ pack ":") . pack . Prelude.init . uriUserInfo
+    userAndPassword = (breakOn $ pack ":") . pack . init . uriUserInfo
     schemeError uri = error $ "was expecting a mongodb scheme, not: " ++ (uriScheme uri) ++ "\n" ++ (show uri)
     invalid = error "could not parse heroku MONGOLAB_URI"
 
--- src/Server.hs
+++ src/Server.hs
@@ -1,10 +1,10 @@
 import Web.Scotty
-import Database
+import Network.HTTP.Types.Status (notFound404)
+
+import Model
 import qualified Database.Persist as DB
 import Database.Persist ((==.))
 import qualified Database.Persist.MongoDB as Mongo
-
-import Network.HTTP.Types.Status (notFound404)
 
 import System.Environment (getEnvironment)
 
@@ -19,47 +19,48 @@ import Control.Monad.Trans (liftIO)
 main :: IO ()
 main = do
     conn <- getEnvDef "MONGOLAB_URI" testMongo >>= return . parseDatabaseUrl
-    let runDB = liftIO . withMongoDBConf (mongoConfFrom conn)
-        from f = f . Mongo.entityVal
-
     port <- getEnvDef "PORT" 8000
-    scotty port $ do
-        get "/" $ text "Nothing to see here *whistles*"
-
-        get "/:slug" $ do
-            graphId <- fromSlug (param "slug")
-            graph <- runDB $ DB.selectFirst [GraphIdent ==. graphId] []
-            case graph of
-                Just g  -> (text $ fromStrict $ graphConfig `from` g) >> respondJson
-                Nothing -> text "404 not found" >> status notFound404
-
-        put "/:slug" $ do
-            graphId <- fromSlug (param "slug")
-            req <- jsonData
-            text req
-            respondJson
-
-        post "/" $ do
-            contents <- fmap (toStrict . decodeUtf8) body
-            graphId <- runDB $ do
-                existingEnt <- DB.selectFirst [] [DB.Desc GraphIdent]
-                let newId = case existingEnt of
-                        Nothing  -> 10000
-                        Just ent -> (graphIdent `from` ent) + 1
-                DB.insert $ Graph newId contents
-                return newId
-            json $ M.fromList [("id" :: String, show $ toSlug $ graphId)]
-
+    scotty port (app conn)
     where
-        toSlug   = encodeWithAlphabet base62
-        fromSlug = fmap (decodeFromAlphabet base62)
-        base62   = concat . transpose $ [['a'..'z'], ['0'..'9'], ['A'..'Z']]
-
         testMongo = "mongodb://test:test@localhost:27017/simple-storage"
-
-        respondJson = setHeader "content-type" "text/json"
         getEnvDef e d =
             getEnvironment >>= return . fromMaybe d . fmap read . lookup e
+
+app conn = do
+    get "/" $ text "Nothing to see here *whistles*"
+
+    get "/:slug" $ do
+        graphId <- fromSlug (param "slug")
+        graph <- runDB $ DB.selectFirst [GraphIdent ==. graphId] []
+        case graph of
+            Just g  -> (text $ fromStrict $ graphConfig `from` g) >> respondJson
+            Nothing -> text "404 not found" >> status notFound404
+
+    put "/:slug" $ do
+        graphId <- fromSlug (param "slug")
+        req <- jsonData
+        text req
+        respondJson
+
+    post "/" $ do
+        contents <- fmap (toStrict . decodeUtf8) body
+        graphId <- runDB $ do
+            existingEnt <- DB.selectFirst [] [DB.Desc GraphIdent]
+            let newId = case existingEnt of
+                    Nothing  -> 10000
+                    Just ent -> (graphIdent `from` ent) + 1
+            DB.insert $ Graph newId contents
+            return newId
+        json $ M.fromList [("id" :: String, show $ toSlug $ graphId)]
+
+    where
+        runDB = liftIO . withMongoDBConf (mongoConfFrom conn)
+        from f = f . Mongo.entityVal
+        respondJson = setHeader "content-type" "text/json"
+
+        base62   = concat . transpose $ [['a'..'z'], ['0'..'9'], ['A'..'Z']]
+        toSlug   = encodeWithAlphabet base62
+        fromSlug = fmap (decodeFromAlphabet base62)
 
         encodeWithAlphabet a 0 = [head a]
         encodeWithAlphabet a i = rest ++ [digit] where
